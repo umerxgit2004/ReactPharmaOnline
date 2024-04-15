9bce7aec91382139949025d95a956f2e6f33b2cd
umerxgit2004 <umer.workplace174@gmail.com>
2024-04-15 16:38:47 +0500

    Inititalized react router dom with header componnent

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,15 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Header from "./Components/Header"
 
 export default function App() {
   return (
-    <div className="bg-red-500">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+        </Routes>
+    </BrowserRouter>
+   
   )
 }
